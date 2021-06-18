221466d9728a48c30fd18d623be079c1df4b29e7
wk3368 <wk3368@163.com>
2021-06-18 16:07:32 +0800

    clear

--- js/about.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function () {
-    $(".about_tab li").click(function () {
-        $(".about_tab li").removeClass("select")
-        $(this).addClass("select")
-        $(".introduce_content").hide()
-        $(".introduce_content").eq($(this).index()).show()
-    })
-});
