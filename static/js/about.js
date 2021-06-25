26674ef6b8716d2aa7a534216a9889b48efacf50
侯婧欢 <31646393+monkeyMall@users.noreply.github.com>
2021-06-25 18:46:38 +0800

    fix: 路由替换等 (#5)
    Co-authored-by: houjinghuan <houjinghuan@zhihu.com>

--- static/js/about.js
+++ static/js/about.js
@@ -1,5 +1,12 @@
 $(document).ready(function () {
+    if(location.hash){
+        $(".about_tab li").removeClass("select")
+        $(`.${location.hash.slice(1)}`).addClass("select")
+        $(".introduce_content").hide()
+        $(".introduce_content").eq($(location.hash).index()).show()
+    }
     $(".about_tab li").click(function () {
+        location.hash = $(this).attr("class")
         $(".about_tab li").removeClass("select")
         $(this).addClass("select")
         $(".introduce_content").hide()
