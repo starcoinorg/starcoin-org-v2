ccaef1daa3084b4f3dd71a2129cda15bfa077477
wk3368 <wk3368@163.com>
2021-07-02 16:45:46 +0800

    change westar labs name

--- publish.sh
+++ publish.sh
@@ -7,3 +7,7 @@ rm -rf resources
 hugo -s . --minify
 
 aws s3 cp ./public s3://starcoin.org/ --recursive
+echo 'Successful upload to s3://starcoin.org/'
+
+aws s3 cp ./public s3://www.starcoin.org/ --recursive
+echo 'Successful upload to s3://www.starcoin.org/'
