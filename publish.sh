bbe5edcf800cc15ee1954a786648476364630c77
wk3368 <wk3368@163.com>
2021-07-05 17:55:22 +0800

    set browser cache =0

--- publish.sh
+++ publish.sh
@@ -4,5 +4,5 @@
 
 find public -name .DS_Store
 
-aws s3 cp ./public s3://starcoin.org/ --recursive
+aws s3 cp ./public s3://starcoin.org/ --recursive --cache-control max-age=0
 echo 'Successful upload to s3://starcoin.org/'
