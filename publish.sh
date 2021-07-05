8a4e7d11429cc1ddc857a54103f2f4368edeb8e2
wk3368 <wk3368@163.com>
2021-07-05 14:45:52 +0800

    do not publish to s3://www.starcoin.org

--- publish.sh
+++ publish.sh
@@ -8,6 +8,3 @@ hugo -s . --minify
 
 aws s3 cp ./public s3://starcoin.org/ --recursive
 echo 'Successful upload to s3://starcoin.org/'
-
-aws s3 cp ./public s3://www.starcoin.org/ --recursive
-echo 'Successful upload to s3://www.starcoin.org/'
