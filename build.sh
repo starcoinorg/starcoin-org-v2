17ba7c884cca4691a0599298a49ee95ace5d55cf
wk3368 <wk3368@163.com>
2021-07-07 14:30:22 +0800

    comment out 'type: *' line, otherwise, hugo will use the news theme

--- build.sh
+++ build.sh
@@ -10,4 +10,7 @@ git submodule init && git submodule update
 
 cd ../../../
 
+# comment out 'type: *' line, otherwise, hugo will use the news theme
+find content/developer/SIPs/ -name index*.md | xargs sed -i 's/^type\:/\# type\:/'
+
 hugo -s . --minify
